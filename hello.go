fff820a40f9a31fa1c26eb1124ad0d297eb092df
ryanemax <ryanemax@hotmail.com>
2015-06-24 02:41:02 +0000

    learn go 0.1

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ryanemax/gostringutil"
+)
+
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!oG , olleH"))
+
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+#repos for learning go language
